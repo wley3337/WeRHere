19c361c419d40626ad978cb688e1b94bfe9a858d
wley3337 <wley3337@gmail.com>
2019-01-21 12:24:01 -0500

    added the SVG images to directory working on getting them to display right

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//icons:
+// import PWAsafety from '/icons/PWAsafety.svg'
+// import PWAfood from '/icons/PWAfood.svg'
+// import PWAhealth from '/icons/PWAhealth.svg'
+// import PWAshelter from '/icons/PWAshelter.svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={'/icons/PWAsafety.svg'} className="triangle-logo" alt="safety"  />
+        <img src={'/icons/PWAfood.svg'} className="triangle-logo" alt="food"  />
+        <img src={'/icons/PWAhealth.svg'} className="triangle-logo" alt="health"  />
+        <img src={'/icons/PWAshelter.svg'} className="triangle-logo" alt="shelter"  />
       </div>
     );
   }
