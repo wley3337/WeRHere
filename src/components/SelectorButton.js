3d5fb8f31c34abdf107bf97661ef9fd3e0326678
wley3337 <wley3337@gmail.com>
2019-07-09 10:00:04 -0400

    backup before shutting down

--- src/components/SelectorButton.js
+++ src/components/SelectorButton.js
@@ -5,6 +5,7 @@ class SelectorButton extends PureComponent {
     render(){
         const {text, clickHandler, selected} = this.props
         const background = selected ? "selected" : "not-selected"
+        //write in a turnery using the prop as argument for the call back. Connect this component and also make two actions, an add and remove
         return (
             <div onClick={clickHandler} className={background}>{text}</div>
         )
