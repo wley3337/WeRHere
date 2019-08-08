9514ab3d5636eacc09614c75b206d151e23096a0
wley3337 <wley3337@gmail.com>
2019-08-08 07:52:24 -0400

    heroku prep

--- src/components/MapFrame.js
+++ src/components/MapFrame.js
@@ -18,7 +18,7 @@ import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl"
 <southbc>38.824801</southbc>
 </bounding> */ 
 
-const mapBoxToken = process.env.REACT_APP_MAPBOX_API
+const mapBoxToken = "pk.eyJ1Ijoid2xleTMzMzciLCJhIjoiY2pzMGQ0N2R5MWkwcTQzbHZ6YnM5bjBwMCJ9.-RiMyhrgweV4aKVG3GfNbA"
 const Map = ReactMapboxGl({
     accessToken: mapBoxToken
   });
